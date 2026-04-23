7596ff232464fbe5f53a65aae85ca00b5f1ce953
Cesar Tablero <cesarjthernandez@gmail.com>
2026-04-23 16:09:26 -0400

    feature/mandoca-1: Added first files for the asg provider module

---
+++ modules/asg-provider/main.tf
@@ -0,0 +1,25 @@
+resource aws_autoscaling_group "asg" {
+
+  launch_template {
+    id = aws_launch_template.launch_template.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = true
+    propagate_at_launch = true
+  }
+
+  vpc_zone_identifier = var.subnets_ids
+
+}
+
+resource aws_launch_template "launch_template" {
+
+  name_prefix = "instance-"
+  image_id    = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group_ids
+
+}
