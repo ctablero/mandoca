46e898266add89565c87385aace66845f84d36f4
Cesar Tablero <cesarjthernandez@gmail.com>
2026-09-02 15:14:08 -0400

    test/scheduler-plus-lambda: Improved the deployment strategy.

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -36,8 +36,8 @@ resource "aws_ecs_service" "service" {
   task_definition = aws_ecs_task_definition.task_definition.arn
   desired_count   = var.desired_count
 
-  deployment_minimum_healthy_percent = 0
-  deployment_maximum_percent         = 100
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
 
   force_new_deployment = true
 
